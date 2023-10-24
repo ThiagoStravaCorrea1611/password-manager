2a7943fa5bd780c7f5b7431ded1efc3333b91b3b
Correa <tocr@gft.com>
2023-10-24 18:08:53 -0300

    Window and canvas initial design

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+
+# Import libraries
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+# Window
+window = Tk()
+window.title("Password Manager")
+window.config(padx = 20, pady = 20)
+
+# Lock canvas
+canvas = Canvas(width = 200, height = 200)
+pomodoro_image = PhotoImage(file = "logo.png")
+canvas.create_image(100, 100, image = pomodoro_image)
+canvas.pack()
+
+# Main loop
+window.mainloop()
